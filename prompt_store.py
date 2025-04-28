3be9dddb775f44b1ddc01157401ffc4b95955bad
yaoling000 <yaolingyufei@gmail.com>
2025-04-29 01:01:39 +1000

    Update prompt_store.py

--- prompt_store.py
+++ prompt_store.py
@@ -28,5 +28,4 @@ def home():
     return "Prompt Store is running!", 200
 
 if __name__ == '__main__':
-    from gunicorn.app.base import BaseApplication
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)
